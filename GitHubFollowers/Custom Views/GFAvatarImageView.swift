cc69b961fc5027ac51a19c3a8b2e33b233a8d886
rammelbalagtas <rammel.balagtas@gmail.com>
2022-06-24 15:49:34 -0400

    Add code for extracting avatar images of followers via the avatar URL

--- GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -28,4 +28,24 @@ class GFAvatarImageView: UIImageView {
         image               = placeholderImage
         translatesAutoresizingMaskIntoConstraints = false
     }
+    
+    func downloadImage(from url: String) {
+        guard
+            let url = URL(string: url)
+        else {return}
+        
+        let task = URLSession.shared.dataTask(with: url)
+        { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            guard let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
+    }
 }
