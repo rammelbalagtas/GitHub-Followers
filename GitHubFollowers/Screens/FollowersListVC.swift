e24b843b4fe9e223d59d41c5777d331db594e5dc
rammelbalagtas <rammel.balagtas@gmail.com>
2022-06-24 11:47:28 -0400

    Build initial setup of collection view and register collection view cells

--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -10,12 +10,33 @@ import UIKit
 class FollowersListVC: UIViewController {
     
     var userName: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        configureViewController()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
+    }
+    
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    private func getFollowers() {
         NetworkManager.shared.getFollowers(userName: userName, page: 1)
         { result in
             switch result {
@@ -26,11 +47,5 @@ class FollowersListVC: UIViewController {
             }
         }
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
-    }
-    
 
 }
