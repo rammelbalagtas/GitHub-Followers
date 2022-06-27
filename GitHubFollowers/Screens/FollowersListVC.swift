cfd32bce29a244cda04d58f2d5b3e7b02f5f8ae7
rammelbalagtas <rammel.balagtas@gmail.com>
2022-06-27 11:38:06 -0400

    Add loading view animation when loading records

--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -48,9 +48,11 @@ class FollowersListVC: UIViewController {
     }
     
     private func getFollowers(userName: String, page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(userName: userName, page: page)
         { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false}
